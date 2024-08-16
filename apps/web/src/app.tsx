638018df655d5947618239669cd121ee05538f5d
MrRevillod <lrevillod2022@alu.uct.cl>
2024-08-16 00:28:55 -0400

    refactor: add code formatting tools
    add prettier and .prettierrc files to the different apps

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,40 +1,34 @@
-
 import React, { useState } from "react"
 
 type ApiResponse = {
-    code: number,
-    message: string
+	code: number
+	message: string
 }
 
 export default function App() {
+	const [apiResponse, setApiResponse] = useState<ApiResponse | null>(null)
 
-    const [apiResponse, setApiResponse] = useState<ApiResponse | null>(null)
+	const handleCreate = async () => {
+		const response = await fetch("http://localhost:3000/api/users", {
+			method: "POST",
+			body: JSON.stringify({ email: "test" }),
+			headers: { "Content-Type": "application/json" },
+		})
 
-    const handleCreate = async () => {
+		setApiResponse((await response.json()) as ApiResponse)
+	}
 
-        const response = await fetch("http://localhost:3000/api/users", {
-            method: "POST",
-            body: JSON.stringify({ email: "test" }),
-            headers: { "Content-Type": "application/json" },
-        })
+	const handleList = async () => {
+		const response = await fetch("http://localhost:3000/api/users")
+		setApiResponse((await response.json()) as ApiResponse)
+	}
 
-        setApiResponse(await response.json() as ApiResponse)
-    }
+	return (
+		<article>
+			<button onClick={() => handleCreate()}> Create Default user </button>
+			<button onClick={() => handleList()}> List users </button>
 
-    const handleList = async () => {
-        const response = await fetch("http://localhost:3000/api/users")
-        setApiResponse(await response.json() as ApiResponse)
-    }
-
-    return (
-
-        <article>
-
-            <button onClick={() => handleCreate()}> Create Default user </button>
-            <button onClick={() => handleList()}> List users </button>
-
-            <section> {JSON.stringify(apiResponse)} </section>
-
-        </article>
-    )
+			<section> {JSON.stringify(apiResponse)} </section>
+		</article>
+	)
 }
